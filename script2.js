c24781ac3cdf6cde18a0f54013e68e89e021a268
LawnGuru1776 <kusscall@gmail.com>
2021-09-09 16:18:38 -0500

    reformated code to be read easier

--- script2.js
+++ script2.js
@@ -23,71 +23,84 @@ let choices = getRandom(wrongChoices);
 let score = 7;
 let highscores = 0;
 let lifeTimeAttempts = 0;
-document.querySelector(`.submit`).addEventListener(`click`, function () {
+const displayMessage = function (message) {
+  document.querySelector(`.message`).textContent = message;
+};
+
+const highScoreNumber = function (highScore) {
+  document.querySelector(`.highScoreNumber`).textContent = highScore;
+};
+
+const documentBody = document.querySelector(`body`);
+const submitBtn = document.querySelector(`.submit`);
+const inputBox = document.querySelector(`.guess`);
+const hiddenHint = document.querySelector(`.mystery`);
+const hiddenHintId = document.getElementById(`mystery1`);
+const playAgain = document.querySelector(`.again`);
+const headerBorder = document.querySelector(`header`);
+const attemptsLeft = document.querySelector(`.score`);
+
+submitBtn.addEventListener(`click`, function () {
   const guess = document.querySelector(`.guess`).value;
   if (!guess) {
     incrementTotAttempts();
     //if nothing is in the input box it populates this line of code
-    document.querySelector(`.guess`).style.color = `blue`;
-    document.querySelector(`.message`).textContent = `You didn't even guess!`;
-    document.querySelector(`body`).style.backgroundColor = `blue`;
-    document.querySelector(`.submit`).style.boxShadow = `5px 5px 5px red`;
-    document.querySelector(`.again`).style.boxShadow = `5px 5px 5px red`;
+    inputBox.style.color = `blue`;
+    displayMessage(`You didn't even guess!`);
+    documentBody.style.backgroundColor = `blue`;
+    submitBtn.style.boxShadow = `5px 5px 5px red`;
+    playAgain.style.boxShadow = `5px 5px 5px red`;
   } else if (guess.toUpperCase() === food.toUpperCase()) {
     //no longer case sensitive
     //if the input === random food populates this code
     if (score > highscores) {
       highscores = score;
-      document.querySelector(`.highScoreNumber`).textContent = highscores;
+      highScoreNumber(highscores);
       saveHighScore(score);
     }
     incrementTotAttempts();
-    document.querySelector(`.guess`).style.color = `green`;
-    document.querySelector(
-      `.message`
-    ).textContent = `Great job! That's correct!`;
-    document.querySelector(`body`).style.backgroundColor = `green`;
-    document.querySelector(`.mystery`).style.backgroundColor = `black`;
-    document.querySelector(`.mystery`).style.color = `#add8e6`;
-    document.querySelector(`.mystery`).style.borderRadius = ``;
-    document.querySelector(`.mystery`).style.width = `100%`;
-    document.querySelector(`.mystery`).textContent = food;
-    document.querySelector(`.submit`).style.boxShadow = `5px 5px 5px aqua`;
-    document.querySelector(`.again`).style.boxShadow = `5px 5px 5px aqua`;
+    inputBox.style.color = `green`;
+    displayMessage(`Great job! That's correct!`);
+    documentBody.style.backgroundColor = `green`;
+    hiddenHint.style.backgroundColor = `black`;
+    hiddenHint.style.color = `#add8e6`;
+    hiddenHint.style.borderRadius = ``;
+    hiddenHint.style.width = `100%`;
+    hiddenHint.textContent = food;
+    submitBtn.style.boxShadow = `5px 5px 5px aqua`;
+    playAgain.style.boxShadow = `5px 5px 5px aqua`;
   } else if (guess != food)
     if (score > 0) {
       //if input != to food populates this
       {
         incrementTotAttempts();
         choices = getRandom(wrongChoices);
-        document.querySelector(`.message`).textContent = choices;
+        displayMessage(choices);
         score--;
-        document.querySelector(`.score`).textContent = score;
-        document.querySelector(`body`).style.backgroundColor = `red`;
-        document.querySelector(`.mystery`).style.color = `var(--mystery)`;
-        document.querySelector(`.submit`).style.boxShadow = `5px 5px 5px aqua`;
-        document.querySelector(`.again`).style.boxShadow = `5px 5px 5px aqua`;
-        document.querySelector(`.guess`).style.color = `red`;
+        attemptsLeft.textContent = score;
+        documentBody.style.backgroundColor = `red`;
+        hiddenHint.style.color = `var(--mystery)`;
+        submitBtn.style.boxShadow = `5px 5px 5px aqua`;
+        playAgain.style.boxShadow = `5px 5px 5px aqua`;
+        inputBox.style.color = `red`;
       }
     } else {
       //this populates when no more attempts exist
-      document.querySelector(`.message`).textContent = `Better luck next time.`;
-      document.querySelector(`body`).style.backgroundColor = `black`;
-      document.querySelector(`.header`).style.color = `aquamarine`;
-      document.querySelector(`.mystery`).style.width = `100%`;
-      document.querySelector(`.mystery`).style.backgroundColor = `teal`;
-      document.querySelector(`.mystery`).textContent = food;
-      document.querySelector(`.mystery`).style.fontSize = `2.5vh`;
-      document.querySelector(`.mystery`).style.color = `white`;
-      document.querySelector(`.mystery`).style.borderRadius = ``;
-      document.querySelector(`.mystery`).textContent = food;
-      document.querySelector(
-        `header`
-      ).style.borderBottom = `7px solid aquamarine`;
-      document.querySelector(`.submit`).style.boxShadow = `5px 5px 5px red`;
-      document.querySelector(`.again`).style.boxShadow = `5px 5px 5px red`;
-      document.getElementById(`mystery1`).textContent = food;
-      document.querySelector(`.guess`).style.color = `black`;
+      displayMessage(`Better luck next time.`);
+      documentBody.style.backgroundColor = `black`;
+      headerBorder.style.color = `aquamarine`;
+      hiddenHint.style.width = `100%`;
+      hiddenHint.style.backgroundColor = `teal`;
+      hiddenHint.textContent = food;
+      hiddenHint.style.fontSize = `2.5vh`;
+      hiddenHint.style.color = `white`;
+      hiddenHint.style.borderRadius = ``;
+      hiddenHint.textContent = food;
+      headerBorder.style.borderBottom = `7px solid aquamarine`;
+      submitBtn.style.boxShadow = `5px 5px 5px red`;
+      playAgain.style.boxShadow = `5px 5px 5px red`;
+      hiddenHintId.textContent = food;
+      inputBox.style.color = `black`;
     }
 });
 
@@ -146,28 +159,28 @@ function defaultText() {
 } //---- end of mystery code----
 
 //code for play again button to reset everything
-document.querySelector(`.again`).addEventListener(`click`, function () {
+playAgain.addEventListener(`click`, function () {
   score = 7;
   food = getRandom(foodChoices);
   choices = getRandom(wrongChoices);
   str = food.substring(0, 2);
-  document.querySelector(`.message`).textContent = `Seriously, Try it!`;
-  document.querySelector(`body`).style.backgroundColor = `rgb(39, 73, 73)`;
-  document.querySelector(`.score`).textContent = score;
-  document.querySelector(`.guess`).value = ``;
-  document.querySelector(`.mystery`).style.color = `var(--mystery)`;
-  document.querySelector(`.mystery`).style.padding = `0.3em`;
-  document.querySelector(`.mystery`).textContent = `???`;
-  document.querySelector(`.mystery`).style.fontSize = `2.5vh`;
-  document.querySelector(`.header`).style.color = `aqua`;
-  document.querySelector(`.mystery`).style.backgroundColor = ``;
+  displayMessage(`Seriously, Try it!`);
+  documentBody.style.backgroundColor = `rgb(39, 73, 73)`;
+  attemptsLeft.textContent = score;
+  inputBox.value = ``;
+  hiddenHint.style.color = `var(--mystery)`;
+  hiddenHint.style.padding = `0.3em`;
+  hiddenHint.textContent = `???`;
+  hiddenHint.style.fontSize = `2.5vh`;
+  headerBorder.style.color = `aqua`;
+  hiddenHint.style.backgroundColor = ``;
   document.querySelector(`header`).style.borderBottom = `7px solid black`;
-  document.querySelector(`.submit`).style.boxShadow = `5px 5px 5px red`;
-  document.querySelector(`.again`).style.boxShadow = `5px 5px 5px red`;
-  document.getElementById('hint').style.backgroundColor = ``;
-  document.getElementById('hint').style.color = ``;
-  document.querySelector(`.guess`).style.color = ``;
-  document.querySelector(`.mystery`).style.width = `var(--mysteryWidth)`;
+  submitBtn.style.boxShadow = `5px 5px 5px red`;
+  playAgain.style.boxShadow = `5px 5px 5px red`;
+  // document.getElementById('hint').style.backgroundColor = ``;
+  // document.getElementById('hint').style.color = ``;
+  inputBox.style.color = ``;
+  hiddenHint.style.width = `var(--mysteryWidth)`;
 });
 
 // Get the input field
@@ -192,13 +205,13 @@ function saveHighScore(maxScore) {
   const everlastingHighScore = Number(localStorage.getItem(`HighScore`));
   if (!everlastingHighScore || everlastingHighScore <= maxScore) {
     localStorage.setItem(`HighScore`, maxScore);
-    document.querySelector(`.highScoreNumber`).textContent = maxScore;
+    highScoreNumber(maxScore);
   }
 }
 //F2 change names everywhere faster
 function init() {
   const findScore = localStorage.getItem(`HighScore`);
-  document.querySelector(`.highScoreNumber`).textContent = findScore || 0;
+  highScoreNumber(findScore || 0);
   const findTotalAttempts = Number(localStorage.getItem(`Attempts`));
   document.querySelector(`.attemptsTotal`).textContent = findTotalAttempts || 0;
   lifeTimeAttempts = findTotalAttempts || 0;
